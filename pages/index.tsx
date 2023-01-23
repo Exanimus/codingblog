b54b410af4a1473c8250b618f594ea067c0571fe
Gabrielius Katkus <gabrielius.katkus@gmail.com>
2023-01-23 20:07:34 +0200

    Added staticProps for homepage to fetch and load blog posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,36 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section>
-        <p className="font-extrabold">[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
-  );
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData()
+	return {
+		props: {
+			allPostsData,
+		},
+	}
+}
+
+export default function Home({ allPostsData }) {
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section>
+				<h2>Blog</h2>
+				<ul>
+					{allPostsData.map(({ id, date, title }) => (
+						<li key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	)
 }
